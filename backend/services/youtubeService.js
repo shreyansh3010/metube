a4b501f291d41082b08f358b7724367ae4efc602
shreyansh3010 <shreyansh.jain561@gmail.com>
2021-10-28 22:22:17 +0530

    Updating apikey after quota gets exhausted

--- backend/services/youtubeService.js
+++ backend/services/youtubeService.js
@@ -1,10 +1,19 @@
+
+const util = require('util');
+
+const redis = require('redis');
+const redisClient = redis.createClient(6379);
+redisClient.get = util.promisify(redisClient.get); // promisify the get function
+
 const { google } = require('googleapis')
 
 // Fetch youtube videos data based on time frame
 const fetchYoutubeVideos = async (publishedAfter, publishedBefore) => {
 
+    let apiKey = await redisClient.get('youtubeApiKey');
+
     let youtubeData = await google.youtube('v3').search.list({
-        key : process.env.GOOGLE_API_KEY,
+        key : JSON.parse(apiKey)['key'],
         part : 'snippet',
         q : process.env.YOUTUBE_SEARCH_PARAM,
         relevanceLanguage: 'en',
@@ -14,6 +23,7 @@ const fetchYoutubeVideos = async (publishedAfter, publishedBefore) => {
     })
 
     return youtubeData['data']
+
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -5,15 +5,41 @@ const port = process.env.PORT || 3000  // Default port we are setting as 3000
 
 // Modules
 const cron = require('node-cron');
-const async = require('async')
+const async = require('async');
+const util = require('util');
+
+// Redis cache
+const redis = require('redis');
+const redisClient = redis.createClient(6379);
+redisClient.get = util.promisify(redisClient.get); // promisify the get function
+redisClient.set = util.promisify(redisClient.set); // promisify the set function
 
 // Imports
 const { fetchYoutubeVideos } = require('./backend/services/youtubeService')
 const { createUpdateVideo } = require('./backend/services/videoService')
 const routes = require('./backend/route')
 
+const youtubeApiKeys = process.env.GOOGLE_API_KEYS.split('|') // Creating api key array
+
 app.set('view engine', 'ejs');
 
+
+/*
+    Saving first key from array to cache
+    {
+        key : current active key
+        index : current active key indexs
+    }
+*/ 
+
+(async () => {
+
+    let newKey = await redisClient.set('youtubeApiKey', JSON.stringify({
+        key : youtubeApiKeys[0],
+        index : 0
+    }))
+
+})()
 
 // 10 Sec cron job to fetch youtube 
 cron.schedule('*/10 * * * * *', async () => {
@@ -24,8 +50,42 @@ cron.schedule('*/10 * * * * *', async () => {
         async.each(youtubeData['items'], createUpdateVideo, (err, result) => { console.log(err, result) })
 
     } catch(err) {
-
+        
         console.log(err);
+
+        if (err.errors && err.errors.length > 0 && err.errors[0]['reason'] == 'quotaExceeded') {
+
+            let expiredKey = await redisClient.get('youtubeApiKey')
+
+            expiredKey = JSON.parse(expiredKey)
+
+            let currentKeyIndex = expiredKey.index
+ 
+            if (expiredKey.index <= youtubeApiKeys.length - 2) {
+
+                // Updating the key with next available key in the array
+                let newKey = await redisClient.set('youtubeApiKey', JSON.stringify({
+                    key : youtubeApiKeys[currentKeyIndex + 1],
+                    index : currentKeyIndex + 1 
+                })) 
+
+                console.log('Key Changed');
+
+            } else {
+                
+                // If expired key is the last one in the array adding 
+                let newKey = await redisClient.set('youtubeApiKey', JSON.stringify({
+                    key : youtubeApiKeys[0],
+                    index : 0
+                }))
+
+                console.log('All keys are exhausted !');
+
+                // We can add sentry or slack channel alert to notify developer 
+
+            }
+        }
+
     }
 })
 
