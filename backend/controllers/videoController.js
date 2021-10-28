0832b8d92409766dc8204d5e2e2c8cf8981dea69
shreyansh3010 <shreyansh.jain561@gmail.com>
2021-10-28 13:14:36 +0530

    Optimise the search with debouncing & caching

--- backend/controllers/videoController.js
+++ backend/controllers/videoController.js
@@ -1,4 +1,8 @@
+const redis = require('redis');
+const redisClient = redis.createClient(6379);
+
 const videoService = require('../services/videoService')
+
 
 // Return list of videos data based on search & pagination
 const fetchVideos = (req, res) => {
@@ -8,31 +12,55 @@ const fetchVideos = (req, res) => {
     let find = req.query.find ? req.query.find : ''
     let sort = req.query.sort ? req.query.sort : 'desc'
 
-    videoService.getVideos(limit, page, find, sort, (err, result) => {
+    // Request Caching
+    try {
+        redisClient.get(req.originalUrl, async (err, response) => {
+
+            if(response) {
+
+                res.send(JSON.parse(response));
+
+            } else {
+                
+                videoService.getVideos(limit, page, find, sort, (err, result) => {
         
-        if (err) {
-            return res.status(500).send({
-                status : 'failure',
-                message : err.message || 'Some thing went wrong'
-            })
-        }
-
-        videoService.getVideosCountBySearch(find, (err, countData) => {
-
-            if (err) {
-                return res.status(500).send({
-                    status : 'failure',
-                    message : err.message || 'Some thing went wrong'
+                    if (err) {
+                        return res.status(500).send({
+                            status : 'failure',
+                            message : err.message || 'Some thing went wrong'
+                        })
+                    }
+            
+                    videoService.getVideosCountBySearch(find, (err, countData) => {
+            
+                        if (err) {
+                            return res.status(500).send({
+                                status : 'failure',
+                                message : err.message || 'Some thing went wrong'
+                            })
+                        }
+            
+                        let response = {
+                            recordsTotal : countData.rows[0].count,
+                            recordsFiltered : countData.rows[0].count,
+                            data : result.rows
+                        }
+                        
+                        redisClient.set(req.originalUrl, JSON.stringify(response), 'EX', 60)  // Setting ttl of 60 sec for cached request
+            
+                        res.send(response)
+                    })
                 })
             }
-    
-            res.send({
-                recordsTotal : countData.rows[0].count,
-                recordsFiltered : countData.rows[0].count,
-                data : result.rows
-            })
         })
-    })
+    } catch(err) {
+
+        return res.status(500).send({
+            status : 'failure',
+            message : err.message || 'Some thing went wrong'
+        })
+        
+    }
 
 }
 
