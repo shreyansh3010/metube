dbe31a2b584fed3952d1eaf8e4bffa15ffac8f7e
shreyansh3010 <shreyansh.jain561@gmail.com>
2021-10-28 22:20:52 +0530

    Change the callback with async/await

--- backend/controllers/videoController.js
+++ backend/controllers/videoController.js
@@ -1,11 +1,13 @@
+const util = require('util');
 const redis = require('redis');
 const redisClient = redis.createClient(6379);
+redisClient.get = util.promisify(redisClient.get); // promisify the get function
 
 const videoService = require('../services/videoService')
 
 
 // Return list of videos data based on search & pagination
-const fetchVideos = (req, res) => {
+const fetchVideos = async (req, res) => {
 
     let limit = req.query.limit ? req.query.limit : 10
     let page = req.query.page ? req.query.page : 1
@@ -14,15 +16,25 @@ const fetchVideos = (req, res) => {
 
     // Request Caching
     try {
-        redisClient.get(req.originalUrl, async (err, response) => {
 
-            if(response) {
+        let response = await redisClient.get(req.originalUrl)
 
-                res.send(JSON.parse(response));
+        if(response) {
 
-            } else {
-                
-                videoService.getVideos(limit, page, find, sort, (err, result) => {
+            res.send(JSON.parse(response));
+
+        } else {
+            
+            videoService.getVideos(limit, page, find, sort, (err, result) => {
+    
+                if (err) {
+                    return res.status(500).send({
+                        status : 'failure',
+                        message : err.message || 'Some thing went wrong'
+                    })
+                }
+        
+                videoService.getVideosCountBySearch(find, (err, countData) => {
         
                     if (err) {
                         return res.status(500).send({
@@ -30,29 +42,20 @@ const fetchVideos = (req, res) => {
                             message : err.message || 'Some thing went wrong'
                         })
                     }
-            
-                    videoService.getVideosCountBySearch(find, (err, countData) => {
-            
-                        if (err) {
-                            return res.status(500).send({
-                                status : 'failure',
-                                message : err.message || 'Some thing went wrong'
-                            })
-                        }
-            
-                        let response = {
-                            recordsTotal : countData.rows[0].count,
-                            recordsFiltered : countData.rows[0].count,
-                            data : result.rows
-                        }
-                        
-                        redisClient.set(req.originalUrl, JSON.stringify(response), 'EX', 60)  // Setting ttl of 60 sec for cached request
-            
-                        res.send(response)
-                    })
+        
+                    let response = {
+                        recordsTotal : countData.rows[0].count,
+                        recordsFiltered : countData.rows[0].count,
+                        data : result.rows
+                    }
+                    
+                    redisClient.set(req.originalUrl, JSON.stringify(response), 'EX', 60)  // Setting ttl of 60 sec for cached request
+        
+                    res.send(response)
                 })
-            }
-        })
+            })
+        }
+
     } catch(err) {
 
         return res.status(500).send({
